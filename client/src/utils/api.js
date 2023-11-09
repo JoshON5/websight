b32325394d670b5a5b126f64ca2e2c65fabae9c0
Houston Davis <133936381+HDavis147@users.noreply.github.com>
2023-11-08 19:51:47 -0600

    searchRepo is exportable, token added to env variables

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,26 +2,26 @@ const { Octokit } = require("octokit/core");
 
 const octokit = new Octokit({
 	auth: process.env.OCTOTOKEN
-  //TODO: add auth token to env
 });
 
-//TODO: bring in featureList from project submission to replace this
-let terms = "";
-const featureList = ["animation", "hover"];
 
-for (let x of featureList) {
-	terms += x + "+";
+const searchRepositories = (featureList) => {
+  let query = "";
+	console.log(featureList);
+
+  for (let x of featureList) {
+    query += x + "+";
+  }
+	console.log(query);
+
+  let queryString = 'q=' + query + 'in:readme&order=desc&per_page=12';
+	console.log(queryString);
+
+  return octokit.request('GET /search/repositories?q=' + queryString, {
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
 }
 
-query = terms.slice(0,-1);
-console.log(query);
-
-const queryString = ('q=' + query + '+in:readme&order=desc&per_page=10');
-
-await octokit.request(`GET /search/repositories?q=Q${queryString}`, {
-	headers: {
-		'X-GitHub-Api-Version': '2022-11-28'
-		}
-})
-
-//TODO: Wrap this in a function and export
+export default searchRepositories;
