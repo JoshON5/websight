680bea3981863c667f0257de8980c7efc0faf3b8
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-11-09 19:25:50 -0600

    heroku staged commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 
-import Nav from './components/Nav';
 
 const client = new ApolloClient({
   uri: '/graphql',
