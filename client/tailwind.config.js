e649b1a2fd92246859133cf55141831cb7a89206
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-11-09 19:05:58 -0600

    heroku deployment

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+<<<<<<< HEAD
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+=======
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+>>>>>>> 948f471 (Nav foundation/tailwind config)
   ],
   theme: {
     extend: {
@@ -15,9 +19,13 @@ export default {
   },
   plugins: [
     // eslint-disable-next-line no-undef
+<<<<<<< HEAD
     require('flowbite/plugin'),
     // eslint-disable-next-line no-undef
     require('@tailwindcss/forms'),
+=======
+    require('flowbite/plugin')
+>>>>>>> 948f471 (Nav foundation/tailwind config)
 ]
 }
 
