2294730579b061eddf5c6014ab9fbe0808d8a176
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-11-11 20:57:43 -0600

    feature/signup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import Image from './../assets/websight-icon.png'
 
 const Login = () => {
     return (
-          <div className="flex w-screen min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="flex w-screen min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gray-300">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               <img
                 className="mx-auto h-10 w-auto"
