5c77788f2b46f273b3078cbf5677e828408b3f87
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-11-11 20:11:12 -0600

    Update ErrorPage.jsx

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -5,12 +5,20 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>oh no!</h1>
-      <p> looks like an unexpected error has occurred :(</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+<main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">OH NO!</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">looks like an unexpected error has occurred :(</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="/"
+              className="rounded-md bg-webTeal px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-webGrey focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </a>
+          </div>
+        </div>
+      </main>
+
   );
 }
