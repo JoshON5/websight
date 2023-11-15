7092805d7bcb26b2c4135e780a432b125b01dd2f
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-11-15 16:21:42 -0600

    fixed authentication and got it working for addRemark

--- client/src/components/Remark/index.jsx
+++ client/src/components/Remark/index.jsx
@@ -1,23 +1,29 @@
 import { useMutation } from '@apollo/client';
-import { ADD_REMARK } from '../../utils/mutations'
-import Auth from '../../utils/auth'
-import { useState } from 'react'
-
+import { ADD_REMARK } from '../../utils/mutations';
+import Auth from '../../utils/auth';
+import { useState } from 'react';
 
 const RemarkForm = ({ projectId }) => {
   const [remarkText, setRemarkText] = useState('');
   const [addRemark, { loading, error }] = useMutation(ADD_REMARK);
 
   const handleRemark = async () => {
+    console.log(Auth.getProfile().data)
     try {
-      const response = await addRemark({
-        variables: {
-          projectId: projectId,
-          remarkText: remarkText,
-          remarkAuthor: Auth.getProfile().data.name
-        },
-      });
-      console.log(response);
+      // Check if the user is an admin before making the mutation request
+      if (Auth.getProfile().data.role === 'ADMIN') {
+        
+        const response = await addRemark({
+          variables: {
+            projectId: projectId,
+            remarkText: remarkText,
+            remarkAuthor: Auth.getProfile().data.name
+          },
+        });
+        console.log(response);
+      } else {
+        console.error('User is not authorized to add a remark.');
+      }
     } catch (error) {
       console.error(error);
     }
