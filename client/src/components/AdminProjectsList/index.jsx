34b0ff52449a49cb5cc2d66ccd79fef3f1827da4
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-11-15 10:06:32 -0600

    UI/ project dashboard for admin and customers

--- client/src/components/AdminProjectsList/index.jsx
+++ client/src/components/AdminProjectsList/index.jsx
@@ -19,22 +19,22 @@ const AllProjects = () => {
 
 
   return (
-    <div>
-      <h2>Complete Project List</h2>
+    <div className='mt-6'>
+      <h2 className='text-3xl font-bold text-webTeal'>Complete Project List:</h2>
       {users.map((user) => (
         <ul key={user._id}>
           {user.projects.map((project) => (
-            <div key={project._id} className="project-card border-4 m-3">
-                            <Link to={`/dashboard/${project._id}`}>
-                <h3>Customer Name: {user.name}</h3>
-                <h3>{project.name}</h3>
-                <p>{project.description}</p>
-              </Link>
+            <div key={project._id} className="project-card border-4  border-webTeal m-3 p-2 bg-webGrey">
+                <Link to={`/dashboard/${project._id}`}>
+                <h3 className='text-webGrey bg-gray-300 px-2 mb-1 text-lg font-semibold'>{project.name}</h3>
+                </Link>
+                <h3 className='text-gray-300 px-2' >Customer Name: {user.name}</h3>
+                <p className='text-gray-300 px-2'>{project.description}</p>
               <div className="features-card mx-3">
-                <h4>Features: </h4>
+                <h4 className='text-gray-300 underline'>Features: </h4>
                 <ul>
                   {project.features.map((feature) => (
-                    <li key={feature.name}>{feature.name}</li>
+                    <li className='text-gray-300 px-2' key={feature.name}>{feature.name}</li>
                   ))}
                 </ul>
               </div>
--- client/src/components/UserProjectsList/index.jsx
+++ client/src/components/UserProjectsList/index.jsx
@@ -19,29 +19,27 @@ const UserProjects = () => {
 
   return (
 <div className='mt-6'> 
-  <h2 className='text-2xl font-bold text-webTeal'>Your Projects: </h2>
+  <h2 className='text-3xl font-bold text-webTeal'>Your Projects: </h2>
   {user.projects.length > 0 ? (
     <div>
       {user.projects.map((project) => (
         // Project card
-        
         <div key={project._id} className="project-card border-4 border-webTeal m-3 p-2 bg-webGrey">
                 <Link to={`/dashboard/${project._id}`}>
-                <h3 className='text-webTeal'>Name: {project.name}</h3>
+                <h3 className='text-webGrey bg-gray-300 px-2 mb-1 text-lg font-semibold'>Name: {project.name}</h3>
                 </Link>
-                <p className='text-gray-300'>Description: {project.description}</p>
+                <p className='text-gray-300 px-2'>Description: {project.description}</p>
           {project.features.length > 0 && (
-            <div className="features-card mx-3">
-              <h4 className='text-gray-300'>Features: </h4>
+            <div className="features-card mx-2">
+              <h4 className='text-gray-300 underline'>Features: </h4>
               <ul>
                 {project.features.map((feature) => (
-                  <li className='text-gray-300' key={feature.name}>{feature.name}</li>
+                  <li className='text-gray-300 px-2' key={feature.name}>{feature.name}</li>
                 ))}
               </ul>
             </div>
           )}
         </div>
-        
       ))}
     </div>
   ) : (
