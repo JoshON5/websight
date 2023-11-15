6e88c0194d4fee304bda18077ea23d1dea55ed6d
shaffachaudhry <134459870+shaffachaudhry@users.noreply.github.com>
2023-11-15 01:19:19 -0600

    UI for project list/your projects

--- client/src/components/UserProjectsList/index.jsx
+++ client/src/components/UserProjectsList/index.jsx
@@ -18,28 +18,30 @@ const UserProjects = () => {
   const { user } = data;
 
   return (
-<div>
-  <h2>Your Projects: </h2>
+<div className='mt-6'> 
+  <h2 className='text-2xl font-bold text-webTeal'>Your Projects: </h2>
   {user.projects.length > 0 ? (
     <div>
       {user.projects.map((project) => (
         // Project card
-        <div key={project._id} className="project-card border-4 m-3">
-                        <Link to={`/dashboard/${project._id}`}>
-                <h3>{project.name}</h3>
-                <p>{project.description}</p>
-              </Link>
+        
+        <div key={project._id} className="project-card border-4 border-webTeal m-3 p-2 bg-webGrey">
+                <Link to={`/dashboard/${project._id}`}>
+                <h3 className='text-webTeal'>Name: {project.name}</h3>
+                </Link>
+                <p className='text-gray-300'>Description: {project.description}</p>
           {project.features.length > 0 && (
             <div className="features-card mx-3">
-              <h4>Features: </h4>
+              <h4 className='text-gray-300'>Features: </h4>
               <ul>
                 {project.features.map((feature) => (
-                  <li key={feature.name}>{feature.name}</li>
+                  <li className='text-gray-300' key={feature.name}>{feature.name}</li>
                 ))}
               </ul>
             </div>
           )}
         </div>
+        
       ))}
     </div>
   ) : (
